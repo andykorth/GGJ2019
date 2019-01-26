50ecc543618a03ed7a5a9fae88ecf468a61c45e7
Andy Korth <andy@howlingmoonsoftware.com>
2019-01-25 22:04:52 -0600

    well it rolls

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public Rigidbody rb;
+    public float gravity = 1;
+    
+    void Update()
+    {
+        Vector3 t = new Vector3(Input.GetAxis("Horizontal"), 0,  Input.GetAxis("Vertical"));
+        rb.AddTorque(t);
+
+
+    }
+}
