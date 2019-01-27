aafae6bc2ec0b99e53da423bb7793a1882d9730d
Andy Korth <andy@howlingmoonsoftware.com>
2019-01-27 16:50:35 -0600

    Player killer bounds set

---
+++ Assets/PlayerKiller.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerKiller : MonoBehaviour
+{
+    private void OnTriggerStay(Collider other)
+    {
+        PlayerController pc = other.gameObject.GetComponent<PlayerController>();
+        if(pc != null){
+           GameManager.i.RespawnPlayer(pc);
+        }
+    }
+}
