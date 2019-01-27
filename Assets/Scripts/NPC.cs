210831c263ff0fe487cb725e7a5db345e3b9e5b0
Andy Korth <andy@howlingmoonsoftware.com>
2019-01-26 18:01:34 -0600

    NPCs roll back to their home

--- Assets/Scripts/NPC.cs
+++ Assets/Scripts/NPC.cs
@@ -6,11 +6,17 @@ using TMPro;
 
 public class NPC : MonoBehaviour
 {
+    public Vector2 startPos;
+
     public int currentLine = 0;
 
     public static List<NPC> allNPCs = new List<NPC>();
+    private Rigidbody rb;
+    public Vector3 moveDirection;
     
     public void OnEnable(){
+        rb = GetComponent<Rigidbody>();
+        startPos = new Vector2(transform.position.x, transform.position.z);
         allNPCs.Add(this);
     }
 
@@ -20,6 +26,13 @@ public class NPC : MonoBehaviour
 
     public float radius = 2.5f;
     public List<string> dialogLines;
+
+    public void Update(){
+        // drive towards start point:
+
+        moveDirection = -(transform.position - new Vector3(startPos.x, transform.position.y, startPos.y));
+        rb.AddTorque(new Vector3(moveDirection.z, 0, -moveDirection.x));
+    }
 
     public void ShowNextLine(){
         if(currentLine < 0){
